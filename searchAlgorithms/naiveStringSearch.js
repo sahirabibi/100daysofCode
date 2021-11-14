5452e07b9301002fd17b50b85cb044133f67ef2a
sahirabibi <sahirabibi96@gmail.com>
2021-11-13 21:32:24 -0500

    Practice naiveSearch algorithm

---
+++ searchAlgorithms/naiveStringSearch.js
@@ -0,0 +1,27 @@
+// check for substring in a search 
+// pseudocode
+// loop over the string
+// loop over the shorter string
+// if char does not match, brak out of the inner loop
+// if you complete the inner loop and find a match increment the count of the matches
+// return the count
+
+function naiveSearch(str, elem) { // lorielol / lol
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === elem[0]) { // l
+            for (let j = 0; j < elem.length; j++) {
+                if (str[i + j] !== elem[j]) { // 
+                    break;
+                }
+                if (j === elem.length - 1) {
+                    count++;
+                }
+            }
+            
+        }
+    }
+    return count;
+}
+
+console.log(naiveSearch('lorie loled', 'lo'))
